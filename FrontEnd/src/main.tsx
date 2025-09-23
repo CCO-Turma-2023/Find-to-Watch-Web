47369aa5229365bb539f4792cfc8e8efb51b8240
Gabriel Alves <109393905+gabrielhdsalves@users.noreply.github.com>
2025-09-23 20:00:41 -0300

    feat: added basic front end structure
    Co-authored-by: Tiago Figueiredo Reis <tiagoreistp@hotmail.com>
    Co-authored-by: Davi <davi2907@users.noreply.github.com>
    Co-authored-by: Juliana <juliana-azevedo@users.noreply.github.com>

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,16 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { PrimeReactProvider } from 'primereact/api';
-import App from './App.tsx';
-import './index.css';
-import "primereact/resources/themes/lara-light-indigo/theme.css"; 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { PrimeReactProvider } from "primereact/api";
+import App from "./views/App.tsx";
+import "./views/styles/index.css";
+import "primereact/resources/themes/lara-light-green/theme.css";
 import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";                      
+import "primeicons/primeicons.css";
+import { BrowserRouter } from "react-router-dom";
+import { MainProvider } from "./views/contexts/mainContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <PrimeReactProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </PrimeReactProvider>
+    <MainProvider>
+      <StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </StrictMode>
+    </MainProvider>
+  </PrimeReactProvider>,
 );
