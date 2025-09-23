91d2661d425fb84c1e702dd3b15203ea8d14e771
Tiago Figueiredo Reis <tiagoreistp@hotmail.com>
2025-09-23 19:34:37 -0300

    feat: added primereact

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';
+import App from './App.tsx';
+import './index.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css"; 
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";                      
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <PrimeReactProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </PrimeReactProvider>
+);
