57895aae1b734fa67243ef37efcedbfc632a6aa2
Tiago Figueiredo Reis <tiagoreistp@hotmail.com>
2025-09-23 18:39:37 -0300

    initial commit

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,10 @@
+
+
+export default function App() {
+
+  return (
+    <div className="h-full bg-black">
+      <p className="text-white font-bold text-center">Test tailwind CSS</p>
+    </div>
+  )
+}
